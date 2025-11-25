20bbf535fcc972f91922931c3384d9000277b285
lurvdroid <49570063+lurvdroid@users.noreply.github.com>
2025-11-25 08:42:24 +0300

    Create posts.js

---
+++ data/posts.js
@@ -0,0 +1,18 @@
+const posts = [
+  {
+    id: '1',
+    title: 'New Look at Christopher Nolan Filming THE ODYSSEY in Iceland',
+    date: 'November 25, 2025',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/3/35/Christopher_Nolan_2013.jpg',
+    content: 'Christopher Nolan was spotted filming scenes for his upcoming epic THE ODYSSEY in the breathtaking landscapes of Iceland. Fans are buzzing with excitement about this new project!',
+  },
+  {
+    id: '2',
+    title: 'Spider-Man: No Way Home Breaks Box Office Records',
+    date: 'November 20, 2025',
+    image: 'https://upload.wikimedia.org/wikipedia/en/9/9a/Spider-Man_No_Way_Home_poster.jpg',
+    content: 'The latest Spider-Man movie has smashed records worldwide, becoming one of the highest-grossing films this year. Critics praise Tom Holland’s performance and jaw-dropping action scenes.',
+  },
+];
+
+export default posts;
